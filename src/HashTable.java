902364b0e561f61744e5b0fb0d5948761705b153
rileytittle <coletittle@ymail.com>
2023-12-07 00:18:52 -0500

    Lots of work being done.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,6 +1,21 @@
-
 public class HashTable {
-
+	private LinkedList[] hashArray;
+	public HashTable(){
+		hashArray = new LinkedList[257];
+		for(int i = 0; i < 257; i++){
+			hashArray[i] = new LinkedList();
+		}
+	}//end HashTable constructor
+	public void insert(String country, long population, long area){
+		String[] nameArray = country.split("");
+		long hashValue = 0;
+		for(int i = 0; i < nameArray.length; i++){
+			hashValue += (int) nameArray[i].charAt(0);
+		}
+		hashValue = hashValue % 257;
+		hashValue = (int) hashValue;
+		hashArray[(int)hashValue].insert(new Node(country, population, area));
+	}
 	private class Node {
 		String name;
 		long population;
@@ -10,10 +25,60 @@ public class HashTable {
 			this.name = name;
 			this.population = population;
 			this.area = area;
+			this.nextNode = null;
 		}//end Node constructor
 		public void printNode() {
-			// print a node’s country name and its population density
+			// print a node’s country name and its population density = total pop / total area
+			System.out.printf("%-38s%-18.4f", this.name, (this.population / this.area));
+			System.out.println();
 		}//end printNode
 	}//end Inner Node class
-	
+	private class LinkedList {
+		public Node first;
+		public Node last;
+		public LinkedList(){
+			first = null;
+			last = null;
+		}//end LinkedList constructor
+		public void insert(Node newNode){
+			if(first == null){
+				first = newNode;
+				last = newNode;
+			}
+			last.nextNode = newNode;
+			last = newNode;
+		}//end insert method
+		public Node findNode(String searchName) {
+			Node current = first;
+			do {
+				if(current.name.equals(searchName)){
+					return current;
+				}
+				else{
+					current = current.nextNode;
+				}
+			} while (current != null);
+			return current;
+		}
+		public void delete(String searchName) {
+			Node nodeToDelete = findNode(searchName);
+			if(nodeToDelete == first){
+				first = first.nextNode;
+			}
+			else if(nodeToDelete == last){
+				Node secondToLastNode = first;
+				while(secondToLastNode.nextNode != last){
+					secondToLastNode = secondToLastNode.nextNode;
+				} 
+				secondToLastNode.nextNode = null;
+			}
+			else{
+				Node nodeBeforeDeleteNode = first;
+				while(nodeBeforeDeleteNode.nextNode != nodeToDelete){
+					nodeBeforeDeleteNode = nodeBeforeDeleteNode.nextNode;
+				}
+				nodeBeforeDeleteNode.nextNode = nodeToDelete.nextNode;
+			}
+		}
+	}//end Inner LinkedList class
 }//end HashTable class
--- src/Project5.java
+++ src/Project5.java
@@ -1,9 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Project5 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("COP3530 Project 5\nHash Tables\nEnter the file name: ");
+		Scanner in = new Scanner(System.in);
+		String fileName = in.nextLine();
+		FileReader fileReader;
+		Scanner fileScanner;
+		boolean fileNotFound = false;
+		do {
+			try {
+				fileReader = new FileReader(fileName);
+				fileScanner = new Scanner(fileReader);
+				fileNotFound = false;
+			} catch (FileNotFoundException e) {
+				System.out.print("File not found. Try again: ");
+				fileName = in.nextLine();
+				fileNotFound = true;
+			}//end catch
+		} while (fileNotFound);//end do while
+		
+		
 	}//end main method
-
 }//end Project5 class
