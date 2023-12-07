6d9435d4c9739c0ecb32bf6c920700291ea72b5a
rileytittle <coletittle@ymail.com>
2023-12-06 22:31:57 -0500

    idk

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,4 +1,19 @@
 
 public class HashTable {
 
-}
+	private class Node {
+		String name;
+		long population;
+		long area;
+		Node nextNode;
+		public Node(String name, long population, long area) {
+			this.name = name;
+			this.population = population;
+			this.area = area;
+		}//end Node constructor
+		public void printNode() {
+			// print a node’s country name and its population density
+		}//end printNode
+	}//end Inner Node class
+	
+}//end HashTable class
