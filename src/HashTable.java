c157bd48e8dd4db7395e938059ad87128d62545a
rileytittle <coletittle@ymail.com>
2023-12-07 14:53:50 -0500

    Finished

--- src/HashTable.java
+++ src/HashTable.java
@@ -7,14 +7,68 @@ public class HashTable {
 		}
 	}//end HashTable constructor
 	public void insert(String country, long population, long area){
-		String[] nameArray = country.split("");
+		int hashValue = hash(country);
+		hashArray[hashValue].insert(new Node(country, population, area));
+	}//end insert method
+	public int find(String country) {
+		int hashValue = hash(country);
+		Node result = hashArray[hashValue].findNode(country);
+		if(result == null){
+			System.out.println(country + " is not in hash table");
+			return -1;
+		}
+		else if(result.name.equals(country)){
+			System.out.println(country + " is found at index " + hashValue + " with population density of " + result.population / result.area);
+			return hashValue;
+		}
+		else{
+			System.out.println(country + " is not in hash table");
+			return -1;
+		}
+	}//end find method
+	public void delete(String country){
+		int hashValue = hash(country);
+		boolean nodeDeleted = hashArray[hashValue].delete(country);
+		if(nodeDeleted){
+			System.out.println(country + " is deleted from hash table");
+		}
+		else{
+			System.out.println(country + " is not a country");
+		}
+	}
+	public void display(){
+		for(int i = 0; i < hashArray.length; i++){
+			if(hashArray[i].first == null){
+				System.out.print(i + ".\tEmpty");
+				System.out.println();
+			}
+			else{
+				System.out.print(i + ".");
+				hashArray[i].printList();
+			}
+		}
+	}
+	public void printEmptyAndCollidedCells(){
+		int emptyCells = 0;
+		int collidedCells = 0;
+		for(int i = 0; i < hashArray.length; i++){
+			if(hashArray[i].first == null){
+				emptyCells++;
+			}
+			else if(hashArray[i].first != hashArray[i].last){
+				collidedCells++;
+			}
+		}
+		System.out.printf("There are %d empty cells and %d colllisions in the hash table\n", emptyCells, collidedCells);
+	}
+	private int hash(String countryName){
+		String[] nameArray = countryName.split("");
 		long hashValue = 0;
 		for(int i = 0; i < nameArray.length; i++){
 			hashValue += (int) nameArray[i].charAt(0);
 		}
 		hashValue = hashValue % 257;
-		hashValue = (int) hashValue;
-		hashArray[(int)hashValue].insert(new Node(country, population, area));
+		return (int) hashValue;
 	}
 	private class Node {
 		String name;
@@ -29,7 +83,7 @@ public class HashTable {
 		}//end Node constructor
 		public void printNode() {
 			// print a node’s country name and its population density = total pop / total area
-			System.out.printf("%-38s%-18.4f", this.name, (this.population / this.area));
+			System.out.printf("\t%-38s%-18d", this.name, (this.population / this.area));
 			System.out.println();
 		}//end printNode
 	}//end Inner Node class
@@ -40,18 +94,23 @@ public class HashTable {
 			first = null;
 			last = null;
 		}//end LinkedList constructor
-		public void insert(Node newNode){
+		public void insert(Node newNode){ 
 			if(first == null){
 				first = newNode;
 				last = newNode;
 			}
+			else{
 			last.nextNode = newNode;
 			last = newNode;
+			}
 		}//end insert method
 		public Node findNode(String searchName) {
 			Node current = first;
 			do {
-				if(current.name.equals(searchName)){
+				if(current == null){
+					return current;
+				}
+				else if(current.name.equals(searchName)){
 					return current;
 				}
 				else{
@@ -60,10 +119,16 @@ public class HashTable {
 			} while (current != null);
 			return current;
 		}
-		public void delete(String searchName) {
+		public boolean delete(String searchName) {
+			boolean nodeDeleted = false;
 			Node nodeToDelete = findNode(searchName);
-			if(nodeToDelete == first){
+			if(nodeToDelete == null){
+				return nodeDeleted;
+			}
+			else if(nodeToDelete == first){
 				first = first.nextNode;
+				nodeDeleted = true;
+				return nodeDeleted;
 			}
 			else if(nodeToDelete == last){
 				Node secondToLastNode = first;
@@ -71,6 +136,8 @@ public class HashTable {
 					secondToLastNode = secondToLastNode.nextNode;
 				} 
 				secondToLastNode.nextNode = null;
+				nodeDeleted = true;
+				return nodeDeleted;
 			}
 			else{
 				Node nodeBeforeDeleteNode = first;
@@ -78,6 +145,15 @@ public class HashTable {
 					nodeBeforeDeleteNode = nodeBeforeDeleteNode.nextNode;
 				}
 				nodeBeforeDeleteNode.nextNode = nodeToDelete.nextNode;
+				nodeDeleted = true;
+				return nodeDeleted;
+			}
+		}
+		public void printList(){
+			Node printNode = first;
+			while(printNode != null){
+				printNode.printNode();
+				printNode = printNode.nextNode;
 			}
 		}
 	}//end Inner LinkedList class
--- src/Project5.java
+++ src/Project5.java
@@ -1,3 +1,4 @@
+import java.awt.desktop.SystemSleepEvent;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -7,8 +8,8 @@ public class Project5 {
 		System.out.print("COP3530 Project 5\nHash Tables\nEnter the file name: ");
 		Scanner in = new Scanner(System.in);
 		String fileName = in.nextLine();
-		FileReader fileReader;
-		Scanner fileScanner;
+		FileReader fileReader = null;
+		Scanner fileScanner = null;
 		boolean fileNotFound = false;
 		do {
 			try {
@@ -21,7 +22,102 @@ public class Project5 {
 				fileNotFound = true;
 			}//end catch
 		} while (fileNotFound);//end do while
+		HashTable theHashTable = new HashTable();
+		fileScanner.nextLine();//discard the headers
+		while(fileScanner.hasNextLine()){
+			String currentLine = fileScanner.nextLine();
+			String[] lineItems = currentLine.split(",");
+			String countryName = lineItems[0];
+			long countryPop = Long.parseLong(lineItems[2]);
+			long countryArea = Long.parseLong(lineItems[4]);
+			theHashTable.insert(countryName, countryPop, countryArea);
+		}
+		System.out.println("There were 128 country records read into the hash table.");
 		
-		
+		int menuOption;
+		boolean continueProgram = true;
+		do {
+			System.out.println("1) Print hash table");
+			System.out.println("2) Delete a country of a given name");
+			System.out.println("3) Insert a country of its name, population, and area");
+			System.out.println("4) Search and print a country and its population density for a given name");
+			System.out.println("5) Print numbers of empty cells and collided cells");
+			System.out.println("6) Exit");
+			System.out.print("Enter your choice: ");
+			String menuChoice = "";
+			boolean invalidChoice = false;
+			do {
+				try {
+					menuChoice = in.nextLine();
+					if(Integer.parseInt(menuChoice) > 6 || Integer.parseInt(menuChoice) < 1) {
+						System.out.print("Invalid choice, enter 1-6: ");
+						invalidChoice = true;
+					}
+					else {
+						invalidChoice = false;
+					}
+				}
+				catch(NumberFormatException e) {
+					System.out.print("Invalid Choice, enter 1-6: ");
+					invalidChoice = true;
+				}
+			}while(invalidChoice);
+			
+			switch(Integer.parseInt(menuChoice)){
+			case 1:
+				theHashTable.display();
+				break;
+			case 2:
+				System.out.print("Enter country name: ");
+				String countryToDelete = in.nextLine();
+				theHashTable.delete(countryToDelete);
+				break;
+			case 3:
+				String nameToInsert = "";
+				long popToInsert = 0;
+				long areaToInsert = 0;
+				String input = "";
+				System.out.print("Enter country name: ");
+				nameToInsert = in.nextLine();
+				System.out.print("Enter country population: ");
+				boolean invalidInput = false;
+				do {
+					input = in.nextLine();
+					try {
+						popToInsert = Long.parseLong(input);
+						invalidInput = false;
+					} catch (NumberFormatException e) {
+						System.out.print("Invalid population. Enter a number: ");
+						invalidInput = true;
+					}
+				}while(invalidInput);
+				System.out.print("Enter country area: ");
+				do {
+					input = in.nextLine();
+					try {
+						areaToInsert = Long.parseLong(input);
+						invalidInput = false;
+					} catch (NumberFormatException e) {
+						System.out.print("Invalid area. Enter a number: ");
+						invalidInput = true;
+					}
+				}while(invalidInput);
+				theHashTable.insert(nameToInsert, popToInsert, areaToInsert);
+				System.out.println(nameToInsert + " is inserted to hash table");
+				break;
+			case 4:
+				System.out.print("Enter country name: ");
+				String countryToFind = in.nextLine();
+				theHashTable.find(countryToFind);
+				break;
+			case 5:
+				theHashTable.printEmptyAndCollidedCells();
+				break;
+			case 6:
+				continueProgram = false;
+				break;
+			}//end switch
+		}while(continueProgram);
+		System.out.println("Have a nice day!");
 	}//end main method
 }//end Project5 class
